bc61dd61f6c01df16844a196ae22cceb20817781
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-11 16:29:36 +0800

    add sector demo

--- social/src/main/java/com/scholar/social/controller/SectorController.java
+++ social/src/main/java/com/scholar/social/controller/SectorController.java
@@ -1,17 +1,27 @@
 package com.scholar.social.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-import static com.scholar.social.util.ControllerParser.*;
-
 /**
  * for sector get
  */
 @RestController
 public class SectorController {
+    @RequestMapping(value = "/getAllSectors",
+            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Map<String, Object> get() {
+        // TODO call service
+        return null;
+    }
+
+    @RequestMapping(value = "/getAllTags",
+            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Map<String, Object> getInfo() {
+        // TODO call service
+        return null;
+    }
 }
