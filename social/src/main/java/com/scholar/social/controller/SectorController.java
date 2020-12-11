bccd39b3d37c451244116125ec9917b024ae7985
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-11 16:24:18 +0800

    move

--- social/src/main/java/com/scholar/social/controller/SectorController.java
+++ social/src/main/java/com/scholar/social/controller/SectorController.java
@@ -14,26 +14,4 @@ import static com.scholar.social.util.ControllerParser.*;
  */
 @RestController
 public class SectorController {
-    @RequestMapping(value = "/isFollowed",
-            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public Map<String, String> get(@RequestBody Map<String, Object> body) {
-        int userId = parseUserId(body);
-        int sectorId = parseSectorId(body);
-        // TODO call service
-        boolean status = false;
-        boolean followed = false;
-        Map<String, String> res = response(status);
-        res.put("followed", String.valueOf(followed));
-        return res;
-    }
-
-    @RequestMapping(value = "/followSector",
-            method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public Map<String, String> set(@RequestBody Map<String, Object> body) {
-        int userId = parseUserId(body);
-        int sectorId = parseSectorId(body);
-        // TODO call service
-        boolean status = false;
-        return response(status);
-    }
 }
