5ad6be236c9699432924a0a92adb751a8cbfc303
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-11 16:02:46 +0800

    add post demo

--- social/src/main/java/com/scholar/social/controller/PostController.java
+++ social/src/main/java/com/scholar/social/controller/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 import static com.scholar.social.util.ControllerParser.*;
@@ -18,9 +19,8 @@ public class PostController {
     @RequestMapping(value = "/deletePost",
             method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public Map<String, String> delete(@RequestBody Map<String, Object> body) {
-        // TODO match front end's body
         int userId = parseUserId(body);
-        int commentId = parseCommentId(body);
+        int postId = parsePostId(body);
         // TODO call service
         boolean status = false;
         return response(status);
@@ -29,9 +29,8 @@ public class PostController {
     @RequestMapping(value = "/reportPost",
             method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public Map<String, String> report(@RequestBody Map<String, Object> body) {
-        // TODO match front end's body
         int userId = parseUserId(body);
-        int commentId = parseCommentId(body);
+        int postId = parsePostId(body);
         String content = parseContent(body);
         // TODO call service
         boolean status = false;
@@ -41,11 +40,13 @@ public class PostController {
     @RequestMapping(value = "/createPost",
             method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public Map<String, String> put(@RequestBody Map<String, Map<String, Object>> body) {
-        // TODO match front end's body
         Map<String, Object> form = body.get("createPostForm");
         int userId = parseUserId(form);
         String name = (String) form.get("postName");
         String content = parseContent(form);
+        int sectorId = parseSectorId(form);
+        List<String> tags = (List<String>) form.get("postTags");
+        int citeId = Integer.parseInt((String) form.get("citeId"));
         // TODO call service
         boolean status = false;
         return response(status);
