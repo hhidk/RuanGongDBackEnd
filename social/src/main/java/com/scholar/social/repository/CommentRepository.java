03c20bc3e93aea2d85fcee99f0dc7c680bfdb380
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-16 11:04:52 +0800

    add CommentRepository

--- social/src/main/java/com/scholar/social/repository/CommentRepository.java
+++ social/src/main/java/com/scholar/social/repository/CommentRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface CommentRepository {
+    void insert(String userId, int postId, String content);
+
+    void delete(String userId, int commentId);
 }
--- social/src/main/java/com/scholar/social/service/CommentService.java
+++ social/src/main/java/com/scholar/social/service/CommentService.java
@@ -1,12 +1,21 @@
 package com.scholar.social.service;
 
+import com.scholar.social.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
     public boolean put(String userId, int postId, String content) {
-        // TODO call repository
-        return false;
+        commentRepository.insert(userId, postId, content);
+        return true;
     }
 
     public boolean report(String userId, int commentId, String content) {
@@ -15,7 +24,7 @@ public class CommentService {
     }
 
     public boolean delete(String userId, int commentId) {
-        // TODO call repository
-        return false;
+        commentRepository.delete(userId, commentId);
+        return true;
     }
 }
