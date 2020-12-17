370792ba17e7e98639020a6fac8585ced53a8c3d
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-17 20:54:40 +0800

    add PostRepository

--- social/src/main/java/com/scholar/social/repository/PostRepository.java
+++ social/src/main/java/com/scholar/social/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.scholar.social.repository;
 
 import com.scholar.social.util.Post;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,24 +10,17 @@ import java.util.List;
 @Repository
 @Mapper
 public interface PostRepository {
-    // TODO INSERT post and return Pk
     int put(Post post);
 
-    // TODO SELECT post
-    Post get(int postId);
+    Post get(@Param("postId") int postId);
 
-    // TODO Tags String
-    void putTags(int postId, String tags);
+    void putTags(@Param("postId") int postId, @Param("tags") String tags);
 
-    // TODO Tags String
-    String getTags(int postId);
+    String getTags(@Param("postId") int postId);
 
-    // TODO DELETE post
-    void delete(int postId);
+    void delete(@Param("postId") int postId);
 
-    // TODO UPDATE view times
-    void updateTimes(int postId);
+    void updateTimes(@Param("postId") int postId);
 
-    // TODO SELECT search
-    List<Post> search(int sectorId, String keyword);
+    List<Post> search(@Param("sectorId") int sectorId, @Param("keyword") String keyword);
 }
