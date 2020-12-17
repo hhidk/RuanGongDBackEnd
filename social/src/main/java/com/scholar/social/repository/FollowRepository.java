d2a29178d38c1e2145829cdf149625b5a80f0b26
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-17 11:03:28 +0800

    add FollowRepository.xml

--- social/src/main/java/com/scholar/social/repository/FollowRepository.java
+++ social/src/main/java/com/scholar/social/repository/FollowRepository.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface FollowRepository {
-    // TODO return the COUNT(*) in SELECT
     int get(String userId, int sectorId);
 
-    // TODO INSERT
     void set(String userId, int sectorId);
 
-    // TODO DELETE
     void unset(String userId, int sectorId);
 }
