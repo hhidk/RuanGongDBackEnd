7fb15766963b9464a87cb03b71471591b9f05254
generalUseAI <general_use_ai@163.com>
2020-12-21 02:03:15 -0800

    fixed some bug

--- literature/src/main/java/com/scholar/literature/service/SearchService.java
+++ literature/src/main/java/com/scholar/literature/service/SearchService.java
@@ -33,19 +33,19 @@ public class SearchService {
 
     public Map<String, Object> advancedSearch(List<SearchItem> list, int start, int end) {
         try {
-            BoolQueryBuilder bq =QueryBuilders.boolQuery();
+            BoolQueryBuilder bq = QueryBuilders.boolQuery();
             SearchRequest searchRequest = new SearchRequest("literature");
             SearchSourceBuilder sb = new SearchSourceBuilder();
             for (SearchItem searchItem : list) {
                 parseQuery(bq, searchItem);
             }
-            bq=bq.filter(QueryBuilders.rangeQuery("year").gte(start).lte(end));
+            bq = bq.filter(QueryBuilders.rangeQuery("year").gte(start).lte(end));
             sb.from(0);
             sb.query(bq);
             sb.size(10000);
             SearchResponse response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
             return resultDeal(response);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -73,135 +73,149 @@ public class SearchService {
     }
 
     private Map<String, Object> resultDeal(SearchResponse response) {
-        //
-        int count = 0;
-        int sum = 0;
-        Map<String, Object> retmap = new HashMap<>();
-        Map<String, Object> answer = new HashMap<>();
-        List<Map<String, Object>> author = new ArrayList<>();
-        List<Map<String, Object>> venue = new ArrayList<>();
-        List<Map<String, Object>> year = new ArrayList<>();
-        List<Map<String, Object>> list1 = new ArrayList<>();
-        List<Map<String, Object>> list2 = new ArrayList<>();
-        List<Map<String, Object>> list3 = new ArrayList<>();
-        List<Literature> litsort = new ArrayList<>();
-        Map<Integer, Integer> years = new TreeMap<>();
-        Map<String, Integer> venues = new TreeMap<>();
-        Map<String, Integer> authors = new TreeMap<>();
+        try {
 
-        /*
-         *  into lits to a list ,sort them and
-         * */
-        long totalhits;
-        SearchHits searchHits = response.getHits();
-        totalhits = searchHits.getTotalHits().value;
-        answer.put("num", totalhits);
-        for (SearchHit searchHit : searchHits) {
-            list3.add(new Literature(searchHit.getSourceAsMap()).retSearchMap());
-            litsort.add(new Literature(searchHit.getSourceAsMap()));
-        }
-        for (Literature literature : litsort) {
-            int y = literature.getYear();
-            String v = literature.getVenue().getRaw();
-            //count times that years appear
-            Integer tmp = years.get(y);
-            if (tmp == null) {
-                years.put(y, 0);
-            } else {
-                years.put(y, tmp + 1);
+
+            int count = 0;
+            int sum = 0;
+            Map<String, Object> retmap = new HashMap<>();
+            Map<String, Object> answer = new HashMap<>();
+            List<Map<String, Object>> author = new ArrayList<>();
+            List<Map<String, Object>> venue = new ArrayList<>();
+            List<Map<String, Object>> year = new ArrayList<>();
+            List<Map<String, Object>> list1 = new ArrayList<>();
+            List<Map<String, Object>> list2 = new ArrayList<>();
+            List<Map<String, Object>> list3 = new ArrayList<>();
+            List<Literature> litsort = new ArrayList<>();
+            Map<Integer, Integer> years = new TreeMap<>();
+            Map<String, Integer> venues = new TreeMap<>();
+            Map<String, Integer> authors = new TreeMap<>();
+
+            /*
+             *  into lits to a list ,sort them and
+             * */
+            long totalhits;
+            SearchHits searchHits = response.getHits();
+            totalhits = searchHits.getTotalHits().value;
+            answer.put("num", totalhits);
+            for (SearchHit searchHit : searchHits) {
+                list3.add(new Literature(searchHit.getSourceAsMap()).retSearchMap());
+                litsort.add(new Literature(searchHit.getSourceAsMap()));
             }
-            //count times that venues appear
-            tmp = venues.get(v);
-            if (tmp == null) {
-                venues.put(v, 0);
-            } else {
-                venues.put(v, tmp + 1);
-            }
-            //count times that authors appear
-            for (LitAuthor literatureAuthor : literature.getAuthors()) {
-                String n = literatureAuthor.getName();
-                tmp = authors.get(n);
-                if (tmp == null) {
-                    authors.put(n, 0);
-                } else {
-                    authors.put(n, tmp + 1);
+            for (Literature literature : litsort) {
+                int y = literature.getYear();
+                String v = literature.getVenue().getRaw();
+                Integer tmp;
+                if (y != 0) {
+                    //count times that years appear
+                    tmp = years.get(y);
+                    if (tmp == null) {
+                        years.put(y, 0);
+                    } else {
+                        years.put(y, tmp + 1);
+                    }
+                }
+                if (v != null) {
+                    //count times that venues appear
+                    tmp = venues.get(v);
+                    if (tmp == null) {
+                        venues.put(v, 0);
+                    } else {
+                        venues.put(v, tmp + 1);
+                    }
+                }
+
+                //count times that authors appear
+                for (LitAuthor literatureAuthor : literature.getAuthors()) {
+                    String n = literatureAuthor.getName();
+                    if (n != null) {
+                        tmp = authors.get(n);
+                        if (tmp == null) {
+                            authors.put(n, 0);
+                        } else {
+                            authors.put(n, tmp + 1);
+                        }
+                    }
                 }
             }
-        }
 
-        List<Map.Entry<String, Integer>> list_venue = new ArrayList<>(venues.entrySet());
-        list_venue.sort(Map.Entry.comparingByValue());
-        List<Map.Entry<Integer, Integer>> list_year = new ArrayList<>(years.entrySet());
-        list_year.sort(Map.Entry.comparingByValue());
-        List<Map.Entry<String, Integer>> list_authors = new ArrayList<>(authors.entrySet());
-        list_authors.sort(Map.Entry.comparingByValue());
+            List<Map.Entry<String, Integer>> list_venue = new ArrayList<>(venues.entrySet());
+            list_venue.sort(Map.Entry.comparingByValue());
+            List<Map.Entry<Integer, Integer>> list_year = new ArrayList<>(years.entrySet());
+            list_year.sort(Map.Entry.comparingByValue());
+            List<Map.Entry<String, Integer>> list_authors = new ArrayList<>(authors.entrySet());
+            list_authors.sort(Map.Entry.comparingByValue());
 
-        //package author
-        for (Map.Entry<String, Integer> list_author : list_authors) {
-            if (count < 5) {
-                Map<String, Object> tmp_author = new HashMap<>();
-                tmp_author.put(list_author.getKey(), list_author.getValue());
-                author.add(tmp_author);
-                count++;
-            } else {
-                sum += list_author.getValue();
+            //package author
+            for (Map.Entry<String, Integer> list_author : list_authors) {
+                if (count < 5) {
+                    Map<String, Object> tmp_author = new HashMap<>();
+                    tmp_author.put(list_author.getKey(), list_author.getValue());
+                    author.add(tmp_author);
+                    count++;
+                } else {
+                    sum += list_author.getValue();
+                }
             }
-        }
-        Map<String, Object> tmp_author = new HashMap<>();
-        tmp_author.put("others", sum);
-        author.add(tmp_author);
-        sum = 0;
-        count = 0;
+            Map<String, Object> tmp_author = new HashMap<>();
+            tmp_author.put("others", sum);
+            author.add(tmp_author);
+            sum = 0;
+            count = 0;
 
-        //package venue
-        for (Map.Entry<String, Integer> ves : list_venue) {
-            if (count < 5) {
-                Map<String, Object> tmp_venue = new HashMap<>();
-                tmp_venue.put(ves.getKey(), ves.getValue());
-                venue.add(tmp_venue);
-                count++;
-            } else {
-                sum += ves.getValue();
+            //package venue
+            for (Map.Entry<String, Integer> ves : list_venue) {
+                if (count < 5) {
+                    Map<String, Object> tmp_venue = new HashMap<>();
+                    tmp_venue.put(ves.getKey(), ves.getValue());
+                    venue.add(tmp_venue);
+                    count++;
+                } else {
+                    sum += ves.getValue();
+                }
             }
-        }
-        Map<String, Object> tmp_venue = new HashMap<>();
-        tmp_venue.put("others", sum);
-        venue.add(tmp_venue);
-        sum = 0;
-        count = 0;
+            Map<String, Object> tmp_venue = new HashMap<>();
+            tmp_venue.put("others", sum);
+            venue.add(tmp_venue);
+            sum = 0;
+            count = 0;
 
-        //package year
-        for (Map.Entry<Integer, Integer> entry : list_year) {
-            if (count < 5) {
-                Map<String, Object> tmp_year = new HashMap<>();
-                tmp_year.put(entry.getKey().toString(), entry.getValue());
-                year.add(tmp_year);
-                count++;
-            } else {
-                sum += entry.getValue();
+            //package year
+            for (Map.Entry<Integer, Integer> entry : list_year) {
+                if (count < 5) {
+                    Map<String, Object> tmp_year = new HashMap<>();
+                    tmp_year.put(entry.getKey().toString(), entry.getValue());
+                    year.add(tmp_year);
+                    count++;
+                } else {
+                    sum += entry.getValue();
+                }
             }
-        }
-        Map<String, Object> tmp_year = new HashMap<>();
-        tmp_year.put("others", sum);
-        year.add(tmp_year);
+            Map<String, Object> tmp_year = new HashMap<>();
+            tmp_year.put("others", sum);
+            year.add(tmp_year);
 
-        litsort.sort(Comparator.comparing(Literature::getN_citation));
-        for (Literature literature : litsort) {
-            list1.add(literature.retSearchMap());
-        }
-        litsort.sort(Comparator.comparing(Literature::getYear));
-        for (Literature literature : litsort) {
-            list2.add(literature.retSearchMap());
-        }
+            litsort.sort(Comparator.comparing(Literature::getN_citation));
+            for (Literature literature : litsort) {
+                list1.add(literature.retSearchMap());
+            }
+            litsort.sort(Comparator.comparing(Literature::getYear));
+            for (Literature literature : litsort) {
+                list2.add(literature.retSearchMap());
+            }
 
-        retmap.put("answer", answer);
-        retmap.put("literatureList1", list1);
-        retmap.put("literatureList2", list2);
-        retmap.put("literatureList3", list3);
-        retmap.put("authorList", author);
-        retmap.put("yearList", year);
-        retmap.put("venueList", venue);
-        return retmap;
+            retmap.put("answer", answer);
+            retmap.put("literatureList1", list1);
+            retmap.put("literatureList2", list2);
+            retmap.put("literatureList3", list3);
+            retmap.put("authorList", author);
+            retmap.put("yearList", year);
+            retmap.put("venueList", venue);
+            return retmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private BoolQueryBuilder parseQuery(BoolQueryBuilder qb, SearchItem item) throws Exception {
@@ -215,11 +229,12 @@ public class SearchService {
                 qb.must(QueryBuilders.matchQuery(s, value));
                 return qb;
             case "OR":
+            case "NULL":
                 qb.should(QueryBuilders.matchQuery(s, value));
                 return qb;
             default:
-                log.error("illegal logical");
-                throw new  Exception("illegal bool keyword") ;
+                log.error("illegal logical: {}", item.getLogical());
+                throw new Exception("illegal bool keyword");
         }
     }
 
