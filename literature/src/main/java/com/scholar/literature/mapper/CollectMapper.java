8e891aa32fd643b4a4ec31c18b9038ce8cc07205
luan6ma <1537011738@qq.com>
2020-12-21 23:02:01 +0800

    getStats写了，但是不保证一定对

--- literature/src/main/java/com/scholar/literature/mapper/CollectMapper.java
+++ literature/src/main/java/com/scholar/literature/mapper/CollectMapper.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
 @Mapper
 public interface CollectMapper {
@@ -11,5 +13,8 @@ public interface CollectMapper {
     int addCollect(@Param("userID") String userID, @Param("literatureID") String literatureID, @Param("title") String title);
 
     int deleteCollect(@Param("userID") String userID, @Param("literatureID") String literatureID);
+
+    // key: literatureID和月份差
+    int getCollectCount(Map<String, Object> map);
 
 }
