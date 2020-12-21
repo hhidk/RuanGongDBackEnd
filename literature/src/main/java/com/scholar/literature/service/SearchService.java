f11c8adbc5de7e378cbb603aa5ea1bc356577511
generalUseAI <general_use_ai@163.com>
2020-12-20 20:18:34 -0800

    finished search function

--- literature/src/main/java/com/scholar/literature/service/SearchService.java
+++ literature/src/main/java/com/scholar/literature/service/SearchService.java
@@ -2,12 +2,14 @@ package com.scholar.literature.service;
 
 import com.scholar.literature.controller.AuthorController;
 import com.scholar.literature.dto.SearchItem;
+import com.scholar.literature.pojo.LitAuthor;
 import com.scholar.literature.pojo.Literature;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.Index;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -19,10 +21,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap.*;
+import java.util.*;
 
 @Service
 public class SearchService {
@@ -31,35 +31,25 @@ public class SearchService {
 
     private static final Logger log = LoggerFactory.getLogger(SearchService.class);
 
-
     public Map<String, Object> advancedSearch(List<SearchItem> list, int start, int end) {
-        // TODO: 12/15/20
-        return null;
-    }
-
-    private Map<String,Object>resultDeal(SearchResponse response){
-        Map<String,Object>retmap=new HashMap<>();
-        Map<String,Object>answer=new HashMap<>();
-        List<Map<String,Object>> author=new ArrayList<>();
-        List<Map<String,Object>> venue=new ArrayList<>();
-        List<Map<String,Object>> year=new ArrayList<>();
-        List<Map<String,Object>>list1 =new ArrayList<>();
-        long totalhits=0;
-        SearchHits searchHits = response.getHits();
-        totalhits=searchHits.getTotalHits().value;
-        answer.put("num",totalhits);
-        for (SearchHit searchHit : searchHits) {
-            list1.add(new Literature(searchHit.getSourceAsMap()).retSearchMap());
+        try {
+            BoolQueryBuilder bq =QueryBuilders.boolQuery();
+            SearchRequest searchRequest = new SearchRequest("literature");
+            SearchSourceBuilder sb = new SearchSourceBuilder();
+            for (SearchItem searchItem : list) {
+                parseQuery(bq, searchItem);
+            }
+            bq=bq.filter(QueryBuilders.rangeQuery("year").gte(start).lte(end));
+            sb.from(0);
+            sb.query(bq);
+            sb.size(10000);
+            SearchResponse response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            return resultDeal(response);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
         }
-
-        retmap.put("answer",answer);
-        retmap.put("literatureList1",list1);
-        retmap.put("literatureList2",list1);
-        retmap.put("authorList",author);
-        retmap.put("yearList",year);
-        retmap.put("venueList",venue);
     }
-
 
     public Map<String, Object> search(SearchItem item) {
         //String key = matchKeyword()
@@ -67,37 +57,174 @@ public class SearchService {
             BoolQueryBuilder bq = QueryBuilders.boolQuery();
             SearchRequest searchRequest = new SearchRequest("literature");
             SearchSourceBuilder sb = new SearchSourceBuilder();
-            sb.size(10000);
-            bq=parseQuery(bq,item);
+            parseQuery(bq, item);
             searchRequest.source(sb);
-            SearchResponse  response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            sb.from(0);
+            sb.query(bq);
+            sb.size(10000);
+            //parse
+            SearchResponse response = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
             return resultDeal(response);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             log.error("unknown err");
             return null;
         }
     }
 
-    private BoolQueryBuilder parseQuery(BoolQueryBuilder qb, SearchItem item) {
-        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+    private Map<String, Object> resultDeal(SearchResponse response) {
+        //
+        int count = 0;
+        int sum = 0;
+        Map<String, Object> retmap = new HashMap<>();
+        Map<String, Object> answer = new HashMap<>();
+        List<Map<String, Object>> author = new ArrayList<>();
+        List<Map<String, Object>> venue = new ArrayList<>();
+        List<Map<String, Object>> year = new ArrayList<>();
+        List<Map<String, Object>> list1 = new ArrayList<>();
+        List<Map<String, Object>> list2 = new ArrayList<>();
+        List<Map<String, Object>> list3 = new ArrayList<>();
+        List<Literature> litsort = new ArrayList<>();
+        Map<Integer, Integer> years = new TreeMap<>();
+        Map<String, Integer> venues = new TreeMap<>();
+        Map<String, Integer> authors = new TreeMap<>();
+
+        /*
+         *  into lits to a list ,sort them and
+         * */
+        long totalhits;
+        SearchHits searchHits = response.getHits();
+        totalhits = searchHits.getTotalHits().value;
+        answer.put("num", totalhits);
+        for (SearchHit searchHit : searchHits) {
+            list3.add(new Literature(searchHit.getSourceAsMap()).retSearchMap());
+            litsort.add(new Literature(searchHit.getSourceAsMap()));
+        }
+        for (Literature literature : litsort) {
+            int y = literature.getYear();
+            String v = literature.getVenue().getRaw();
+            //count times that years appear
+            Integer tmp = years.get(y);
+            if (tmp == null) {
+                years.put(y, 0);
+            } else {
+                years.put(y, tmp + 1);
+            }
+            //count times that venues appear
+            tmp = venues.get(v);
+            if (tmp == null) {
+                venues.put(v, 0);
+            } else {
+                venues.put(v, tmp + 1);
+            }
+            //count times that authors appear
+            for (LitAuthor literatureAuthor : literature.getAuthors()) {
+                String n = literatureAuthor.getName();
+                tmp = authors.get(n);
+                if (tmp == null) {
+                    authors.put(n, 0);
+                } else {
+                    authors.put(n, tmp + 1);
+                }
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list_venue = new ArrayList<>(venues.entrySet());
+        list_venue.sort(Map.Entry.comparingByValue());
+        List<Map.Entry<Integer, Integer>> list_year = new ArrayList<>(years.entrySet());
+        list_year.sort(Map.Entry.comparingByValue());
+        List<Map.Entry<String, Integer>> list_authors = new ArrayList<>(authors.entrySet());
+        list_authors.sort(Map.Entry.comparingByValue());
+
+        //package author
+        for (Map.Entry<String, Integer> list_author : list_authors) {
+            if (count < 5) {
+                Map<String, Object> tmp_author = new HashMap<>();
+                tmp_author.put(list_author.getKey(), list_author.getValue());
+                author.add(tmp_author);
+                count++;
+            } else {
+                sum += list_author.getValue();
+            }
+        }
+        Map<String, Object> tmp_author = new HashMap<>();
+        tmp_author.put("others", sum);
+        author.add(tmp_author);
+        sum = 0;
+        count = 0;
+
+        //package venue
+        for (Map.Entry<String, Integer> ves : list_venue) {
+            if (count < 5) {
+                Map<String, Object> tmp_venue = new HashMap<>();
+                tmp_venue.put(ves.getKey(), ves.getValue());
+                venue.add(tmp_venue);
+                count++;
+            } else {
+                sum += ves.getValue();
+            }
+        }
+        Map<String, Object> tmp_venue = new HashMap<>();
+        tmp_venue.put("others", sum);
+        venue.add(tmp_venue);
+        sum = 0;
+        count = 0;
+
+        //package year
+        for (Map.Entry<Integer, Integer> entry : list_year) {
+            if (count < 5) {
+                Map<String, Object> tmp_year = new HashMap<>();
+                tmp_year.put(entry.getKey().toString(), entry.getValue());
+                year.add(tmp_year);
+                count++;
+            } else {
+                sum += entry.getValue();
+            }
+        }
+        Map<String, Object> tmp_year = new HashMap<>();
+        tmp_year.put("others", sum);
+        year.add(tmp_year);
+
+        litsort.sort(Comparator.comparing(Literature::getN_citation));
+        for (Literature literature : litsort) {
+            list1.add(literature.retSearchMap());
+        }
+        litsort.sort(Comparator.comparing(Literature::getYear));
+        for (Literature literature : litsort) {
+            list2.add(literature.retSearchMap());
+        }
+
+        retmap.put("answer", answer);
+        retmap.put("literatureList1", list1);
+        retmap.put("literatureList2", list2);
+        retmap.put("literatureList3", list3);
+        retmap.put("authorList", author);
+        retmap.put("yearList", year);
+        retmap.put("venueList", venue);
+        return retmap;
+    }
+
+    private BoolQueryBuilder parseQuery(BoolQueryBuilder qb, SearchItem item) throws Exception {
         String s = matchKeyword(item.getType());
         String value = item.getValue();
-        if (item.getLogical() == "NOT") {
-            qb = qb.mustNot(QueryBuilders.matchQuery(s, value));
-            return qb;
-        } else if (item.getLogical() == "AND") {
-            qb = qb.must(QueryBuilders.matchQuery(s, value));
-        } else if (item.getLogical() == "OR") {
-            qb = qb.should(QueryBuilders.matchQuery(s, value));
-        }else {
-            log.error("illegal logical");
+        switch (item.getLogical()) {
+            case "NOT":
+                qb.mustNot(QueryBuilders.matchQuery(s, value));
+                return qb;
+            case "AND":
+                qb.must(QueryBuilders.matchQuery(s, value));
+                return qb;
+            case "OR":
+                qb.should(QueryBuilders.matchQuery(s, value));
+                return qb;
+            default:
+                log.error("illegal logical");
+                throw new  Exception("illegal bool keyword") ;
         }
-        return null;
     }
 
     private String matchKeyword(String s) {
-        // TODO: 12/16/20
+        // TODO: unhandled FI
         switch (s) {
             case "SU": {
                 return "keywords";
@@ -112,7 +239,7 @@ public class SearchService {
                 return "authors.id";
             }
             case "FI": {
-                return null;
+                return "unreachable place";
             }
             case "AF": {
                 return "authors.org";
