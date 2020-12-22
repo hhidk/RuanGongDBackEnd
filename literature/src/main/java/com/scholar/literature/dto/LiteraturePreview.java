96bf5af37c80f05747455f7c1b1da911508070b8
hhidk <295809364@qq.com>
2020-12-22 13:16:07 +0800

    Collect

---
+++ literature/src/main/java/com/scholar/literature/dto/LiteraturePreview.java
@@ -0,0 +1,12 @@
+package com.scholar.literature.dto;
+
+import lombok.Data;
+
+@Data
+public class LiteraturePreview {
+    private String literatureID;
+    private String title;
+    private String collectTime;
+    private String year;
+    private String venue;
+}
