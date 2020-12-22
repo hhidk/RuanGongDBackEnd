64007cdefb444141f2db4af416be72acef04af30
generalUseAI <general_use_ai@163.com>
2020-12-22 05:22:26 -0800

    finished function getAuthorPublishState

--- literature/src/main/java/com/scholar/literature/controller/LiteratureController.java
+++ literature/src/main/java/com/scholar/literature/controller/LiteratureController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -83,9 +84,9 @@ public class LiteratureController {
     }
 
     @RequestMapping("/getHighCitation")
-    public List<Map<String, Object>> getHighCitation() {
+    public Map<String,Object> getHighCitation() {
         try {
-      return   literatureService.getHighCitation();
+      return (Map<String, Object>) new HashMap<String,Object>().put("ciationList",literatureService.getHighCitation());
         } catch (Exception e) {
             e.printStackTrace();
             return null;
