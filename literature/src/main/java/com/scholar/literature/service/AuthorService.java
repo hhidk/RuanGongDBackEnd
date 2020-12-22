74990e6de2b264f58093448ab986ac5ea8f75c8c
generalUseAI <general_use_ai@163.com>
2020-12-22 03:48:02 -0800

    finished function getAuthorPublishState

--- literature/src/main/java/com/scholar/literature/service/AuthorService.java
+++ literature/src/main/java/com/scholar/literature/service/AuthorService.java
@@ -19,10 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,30 +30,41 @@ public class AuthorService {
 
     @Autowired
     RestHighLevelClient restHighLevelClient;
-    public List<Map<String,Object>> getAuthors(String authorName) {
+
+    public List<Map<String, Object>> getAuthors(String authorName) {
 
         try {
-            List<Map<String,Object>> maps=new ArrayList<>();
+            List<Map<String, Object>> maps = new ArrayList<>();
             SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-            sourceBuilder.query(QueryBuilders.matchQuery("name",authorName));
+            sourceBuilder.query(QueryBuilders.matchQuery("name", authorName));
             sourceBuilder.size(10000);
             SearchRequest searchRequest = new SearchRequest("author").source(sourceBuilder);
-            SearchResponse searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+            SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
             SearchHits searchHits = searchResponse.getHits();
             for (SearchHit searchHit : searchHits) {
-            Author author=new Author(searchHit.getSourceAsMap());
-            Map<String,Object> map =new HashMap<>();
-            map.put("authorID",author.getId());
-            map.put("name",author.getName());
-            maps.add(map);
+                Author author = new Author(searchHit.getSourceAsMap());
+                Map<String, Object> map = new HashMap<>();
+                map.put("authorID", author.getId());
+                map.put("name", author.getName());
+                maps.add(map);
             }
             return maps;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-    public List<String>getRelatedAuthor(String venue){
+
+    public List<Integer> getPublishState() {
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            tmp.add((int)(Math.random()*3));
+        }
+        return tmp;
+    }
+
+
+    public List<String> getRelatedAuthor(String venue) {
         log.info("In func:getRelatedAuthor : venue = {}", venue);
         Map<String, Object> ret = new HashMap<>();
         List<String> authorID = new ArrayList<>();
@@ -71,18 +79,18 @@ public class AuthorService {
                 log.info("get literature, ID = {} ", searchHit.getId());
                 authorID.addAll(
                         new Literature(
-                        searchHit.getSourceAsMap())
+                                searchHit.getSourceAsMap())
                                 .getAuthors()
                                 .stream()
                                 .map(LitAuthor::getId).collect(Collectors.toList())
-                        );
+                );
             }
             //delete repeated factors
-            authorID=authorID.
+            authorID = authorID.
                     stream().
                     distinct().
                     collect(Collectors.toList());
-            log.info("authors are {}",authorID);
+            log.info("authors are {}", authorID);
             return authorID;
         } catch (Exception e) {
             e.printStackTrace();
