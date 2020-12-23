114bfb2dd9726ec4a4d1ccafafbd6515f1bbd10a
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-23 17:00:01 +0800

    fix CORS

--- social/src/main/java/com/scholar/social/config/CorsConfig.java
+++ social/src/main/java/com/scholar/social/config/CorsConfig.java
@@ -1,9 +1,10 @@
 package com.scholar.social.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
