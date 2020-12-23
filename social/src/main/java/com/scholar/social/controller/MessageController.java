453a835f35912cc534548f5398fe30c94ed85041
Holmium Jiang <42644820+HolmiumTS@users.noreply.github.com>
2020-12-24 00:34:57 +0800

    add log

--- social/src/main/java/com/scholar/social/controller/MessageController.java
+++ social/src/main/java/com/scholar/social/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.scholar.social.controller;
 
 import com.scholar.social.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import static com.scholar.social.util.ControllerParser.response;
 @RestController
 @CrossOrigin
 public class MessageController {
+    private static final Logger log = LoggerFactory.getLogger(MessageController.class);
     private final MessageService messageService;
 
     @Autowired
@@ -23,6 +26,7 @@ public class MessageController {
 
     @PostMapping(value = "/createConsultation", produces = "application/json;charset=UTF-8")
     public Map<String, String> put(@RequestBody Map<String, Object> body) {
+        log.debug("body: {}", body);
         String senderId = (String) body.get("senderId");
         String receiverId = (String) body.get("receiverId");
         String content = (String) body.get("text");
--- social/src/main/java/com/scholar/social/service/MessageService.java
+++ social/src/main/java/com/scholar/social/service/MessageService.java
@@ -1,11 +1,14 @@
 package com.scholar.social.service;
 
 import com.scholar.social.repository.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+    private static final Logger log = LoggerFactory.getLogger(MessageService.class);
     private final MessageRepository messageRepository;
 
     @Autowired
@@ -14,6 +17,7 @@ public class MessageService {
     }
 
     public boolean put(String senderId, String receiverId, String content) {
+        log.debug("s: {}, r: {}, content: {}", senderId, receiverId, content);
         messageRepository.put(senderId, receiverId, content);
         return true;
     }
