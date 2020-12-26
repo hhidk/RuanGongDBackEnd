7b5f06f73bf38c62b8c9b38a2dbdd966d4271294
generalUseAI <general_use_ai@163.com>
2020-12-26 05:29:37 -0800

    modified pom.xml

--- social/src/test/java/com/scholar/social/SocialApplicationTests.java
+++ social/src/test/java/com/scholar/social/SocialApplicationTests.java
@@ -6,14 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SocialApplicationTests {
 
-<<<<<<< HEAD
-    @Test
-    void contextLoads() {
-    }
-=======
-	@Test
-	void contextLoads() {
-	}
->>>>>>> f82ecd20bb6d44269061cc42c300dd9a703e4652
-
 }
